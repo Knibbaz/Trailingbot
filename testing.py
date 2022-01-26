6081c270c266d4ce043518576e709343114f1fc3
Babbink <info@basabbink.nl>
2022-01-26 17:34:30 +0100

    Added multiple coins that can be tested (Build an automatic test #2)

--- testing.py
+++ testing.py
@@ -1,15 +1,16 @@
 from trailingbot import trading
 import requests
 from datetime import datetime
-import numpy as np
 
 ## Bot settings
 buySide = True
 currentOrder = None
-botMoney = 1000 
+botMoney = 1000
+
+markets = ["BTCUSDT", "BTCEUR", "ETHUSDT", "ETHEUR"]
 
 day = 86400 ## Timestamp for one day
-start = int(datetime.now().timestamp()) - (day * 365) ## 30 days ago
+start = int(datetime.now().timestamp()) - (day * 30) ## amount days ago for today
 end = int(datetime.now().timestamp()) ## Today
 interval = '1d' ## Timestamp to trade on
 market = "ETH" ## Market to trade on
@@ -27,46 +28,52 @@ def tradingTesting():
     randomSettings(maxStopLimit, stepsStopLimit, False)
     
     ## Testing price fixed
-    # maxStopLimit = 500
-    # stepsStopLimit = 100
-    # randomSettings(maxStopLimit, stepsStopLimit, True)
+    maxStopLimit = 1000
+    stepsStopLimit = 100
+    randomSettings(maxStopLimit, stepsStopLimit, True)
 
 def randomSettings(maxStopLimit, stepsStopLimit, priceFixed):
     BTSL = 0
     while BTSL <= maxStopLimit:
-        BTSL += stepsStopLimit
         STSL = 0
+        
         while STSL <= maxStopLimit:
+            randomCoins(BTSL, STSL, priceFixed)
             STSL += stepsStopLimit
-            randomDate(BTSL, STSL, priceFixed)
-            
-def randomDate(BTSL, STSL, priceFixed):
+        
+        BTSL += stepsStopLimit
+     
+def randomCoins(BTSL, STSL, priceFixed):
+    for market in markets:
+        randomDate(market, BTSL, STSL, priceFixed)
+           
+def randomDate(market, BTSL, STSL, priceFixed):
     # Get the chartdata
-    chart = requests.get("https://api.binance.com/api/v3/klines?symbol=" + market + "EUR&interval=" + interval + "&limit=1000" + "&startTime=" + str(start * 1000) + "&endTime=" + str(end * 1000)).json()
-    
+    chart = requests.get("https://api.binance.com/api/v3/klines?symbol=" + market + "&interval=" + interval + "&limit=1000" + "&startTime=" + str(start * 1000) + "&endTime=" + str(end * 1000)).json()
+
     # Run the date forwards
     testingDate = start
     while testingDate < end:
         trades, resultMoney = trading(chart, buySide, currentOrder, botMoney, BTSL, STSL, priceFixed)
-        if resultMoney > 1000: 
+        if resultMoney > botMoney: 
             tradesTesting.append(trades)
-            settingsTesting.append({"testingDate": testingDate, "end": end, "BTSL": BTSL, "STSL": STSL, "priceFixed": priceFixed})
+            settingsTesting.append({"market": market, "testingDate": testingDate, "end": end, "BTSL": BTSL, "STSL": STSL, "priceFixed": priceFixed})
             resultMoneyTesting.append(resultMoney)
         testingDate += day
         if len(chart) > 0: del chart[0]
         
     # Get the chartdata
-    chart = requests.get("https://api.binance.com/api/v3/klines?symbol=" + market + "EUR&interval=" + interval + "&limit=1000" + "&startTime=" + str(start * 1000) + "&endTime=" + str(end * 1000)).json()
-    
+    chart = requests.get("https://api.binance.com/api/v3/klines?symbol=" + market + "&interval=" + interval + "&limit=1000" + "&startTime=" + str(start * 1000) + "&endTime=" + str(end * 1000)).json()
+
     # Run the date backwards
     testingDate = end
     while start < testingDate:
         if len(chart) > 0: del chart[-1]
         testingDate -= day
         trades, resultMoney = trading(chart, buySide, currentOrder, botMoney, BTSL, STSL, priceFixed)
-        if resultMoney > 1000: 
+        if resultMoney > botMoney: 
             tradesTesting.append(trades)
-            settingsTesting.append({"testingDate": testingDate, "end": end, "BTSL": BTSL, "STSL": STSL, "priceFixed": priceFixed})
+            settingsTesting.append({"market": market, "testingDate": testingDate, "end": end, "BTSL": BTSL, "STSL": STSL, "priceFixed": priceFixed})
             resultMoneyTesting.append(resultMoney)
 
 tradingTesting()
