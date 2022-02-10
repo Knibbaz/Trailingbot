7c09956721bdec6e2c88cff09cd3b0ea4260dbe7
Babbink <info@basabbink.nl>
2022-02-10 18:32:31 +0100

    Added extra helper functions

--- bitvavo.py
+++ bitvavo.py
@@ -20,15 +20,40 @@ bitvavo = Bitvavo({
 })
 
 def cancelOrder(coin, orderId): return bitvavo.cancelOrder(coin + '-EUR', orderId)
-
 def getBalance(symbol): return bitvavo.balance({'symbol': symbol})[0]
-# def getBalance(): return bitvavo.balance({})
 def getChartCandles(coin, interval): return bitvavo.candles(coin + '-EUR', interval, {})
 def getOrder(coin, orderId): return bitvavo.getOrder(coin + '-EUR', orderId)
 def getOpenOrders(): return bitvavo.ordersOpen({})
 
-def placeLimitOrder(coin, side, amount, targetPrice):
-  return bitvavo.placeOrder(coin + '-EUR', side, 'limit', { 'amount': amount, 'price': targetPrice })
-
 def placeStopLossOrder(coin, side, amount, targetPrice):
+  print(amount)
+  amount = float(getRightDecimals(amount))
+  targetPrice = int(getRightDecimals(targetPrice))
+  
   return bitvavo.placeOrder(coin + '-EUR', side, 'stopLossLimit', { 'amount': amount, 'price': targetPrice, 'triggerAmount': targetPrice, 'triggerType': 'price', 'triggerReference': 'lastTrade'})
+
+## Extra functions
+def getOrderDetailsSorted(order):
+  filledAmount = 0
+  filledPrice = []
+  payedFee = 0
+  
+  fills = order['fills']
+  
+  for fill in fills :
+    filledAmount += float(fill['amount'])
+    filledPrice.append(float(fill['price']))
+    payedFee += float(fill['fee'])
+  
+  if len(filledPrice) > 0: filledPrice = sum(filledPrice) / len(filledPrice)
+  else: filledPrice = 0
+  
+  return filledAmount, filledPrice, payedFee
+
+def getRightDecimals(input):
+  if input >= 100000: return round(input, 0)
+  elif input >= 10000: return round(input, 1)
+  elif input >= 1000: return round(input, 2)
+  elif input >= 100: return round(input, 3)
+  elif input >= 10: return round(input, 4)
+  else: return round(input, 5)
